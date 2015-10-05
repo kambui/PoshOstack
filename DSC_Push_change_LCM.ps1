4062181f951f54f03d6f9b7b94b7e2e344ee39ee
Kambui <kambui@prodevans.com>
2015-10-06 01:07:47 +0530

    New DSC Working Files added.wow! Learnt DSC today!!

---
+++ DSC_Push_change_LCM.ps1
@@ -0,0 +1,14 @@
+﻿[DSCLocalConfigurationManager()]
+configuration testConfig {
+    node KAMBUI-NOTE {
+        Settings {
+            AllowModuleOverwrite = $true
+            RefreshMode = 'push'
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+        }
+           
+    }
+
+}
+
+testConfig -OutputPath C:\
---
+++ DSC_Push_create_file_config.ps1
@@ -0,0 +1,11 @@
+﻿configuration CreateFileWithMyName {
+    Node $env:COMPUTERNAME {
+         File AviksFile.txt {
+            DestinationPath = 'C:\AviksFile.txt'
+            Force = $true
+            Contents = 'Hi This File is Created by DSC Automatically'
+         }
+    }
+}
+
+CreateFileWithMyName -OutputPath C:\
